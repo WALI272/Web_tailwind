c4814f7b8ea441db7700bb3165dde71ae7b8bb54
Lucas Walica <Projectwali272@gmail.com>
2023-09-05 14:08:52 +0200

    first commit

---
+++ react-tailwind-darkmode/src/App.jsx
@@ -0,0 +1,38 @@
+import {useEffect, useState} from 'react'
+
+function App() {
+
+  const [theme, setTheme] = useState(() => {
+    if (window.matchMedia('(prefers-color-sheme: dark)').matches) {
+      return "dark";
+    }
+    return "light";
+  });
+
+  useEffect(() => {
+    if (theme == "dark") {
+      document.querySelector("html").classList.add("dark")
+    }
+    else if (theme == "light") {
+      document.querySelector("html").classList.remove("dark")
+    }
+    
+  }, [theme])
+
+  const handleChangeTheme = () => {
+    setTheme(prevTheme => prevTheme === "light" ? "dark" : "light")
+  }
+
+
+  return (
+    <div className="h-screen flex justify-center items-center dark:bg-neutral-900">
+      <button className="bg-slate-200 px-4 py-2 rounded hover:bg-slate-300 
+        dark:bg-slate-950 dark:text-white dark:hover:bg-slate-800"
+        onClick={handleChangeTheme}>
+        Change Theme
+      </button>
+    </div>
+  )
+}
+
+export default App
